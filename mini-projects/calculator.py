3c4fc3283bae1fa7c438beacf8fa30d8395ded55
Divyesh Mahapatruni <divyeshmahapatruni@gmail.com>
2026-05-06 05:46:32 +0000

    updated calculator project

--- mini-projects/calculator.py
+++ mini-projects/calculator.py
@@ -1,23 +1,36 @@
 while True:
-    a = float(input("Enter 1st value:-"))
-    b = float(input("Enter 2nd value:-"))
 
     op = input("Enter operation(+,-,*,/) or q to quit:-")
 
-    if op == 'q':
+    if op.lower() == 'q':
         break
+    if op not in ['+','-','*','/']:
+        print("Invalid operator")
+        print()
+        continue
+
+    try:
+        a = float(input("Enter 1st value:-"))
+        b = float(input("Enter 2nd value:-"))
+    except ValueError:
+        print("Invalid input")
+        print()
+        continue
 
     if op == '+':
         print(a+b)
+        print()
     elif op == '-':
         print(a-b)
+        print()
     elif op == '*':
         print(a*b)
+        print()
     elif op == '/':
         if b!=0:
             print(a/b)
+            print()
         else:
             print("Cannot divide by zero\n")
-    else:
-        print("Invallid Operation\n")
+            continue
 
